01b8cbdc0dbd760533954906a6a2ecc40db20bad
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 21:06:15 -0300

    alterado o nome da variavel aux para num

--- SomaParImpar/Program.cs
+++ SomaParImpar/Program.cs
@@ -1,15 +1,15 @@
-﻿int somaImpar = 0, somaPar = 0, aux = 0;
+﻿int somaImpar = 0, somaPar = 0, num = 0;
 
 do
 {
-    if (aux % 2 == 0)
-        somaPar += aux;
+    if (num % 2 == 0)
+        somaPar += num;
     else
-        somaImpar += aux;
+        somaImpar += num;
 
     Console.WriteLine("informe um numero de 1 a 999 para ser somado: ");
-    aux = int.Parse(Console.ReadLine());
-} while (aux > 0 && aux < 1000);
+    num = int.Parse(Console.ReadLine());
+} while (num > 0 && num < 1000);
 
 Console.WriteLine("par: " + somaPar);
 Console.WriteLine("impar: " + somaImpar);
